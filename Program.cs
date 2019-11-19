3873dfdbe5de8d9af863598c4fcead19bd867914
unknown <mariobud@gmail.com>
2019-11-19 09:43:40 -0600

    Second phase done

--- Program.cs
+++ Program.cs
@@ -14,18 +14,44 @@ namespace heist
             while (heisterList.Count < 4)
             {
                 string name;
-                int skill;
-                decimal courage;
+                string skillString;
+                string courageString;
+
+                int skillLevel = 0;
+                decimal courageFactor = 0.0M;
+
                 Console.WriteLine($"Number of heisters: {heisterList.Count}");
                 Console.WriteLine("What is your name?");
                 name = Console.ReadLine();
                 if (name == "")
                     break;
                 Console.WriteLine("What is your skill level?");
-                skill = int.Parse(Console.ReadLine());
+                skillString = Console.ReadLine();
+
+                try{
+                    skillLevel = int.Parse(skillString);
+                }
+
+                catch(Exception ex){
+
+                    Console.WriteLine($"{skillString} is not a valid skill. Using the default of 10.");
+                    skillLevel = 10;
+                }
+
                 Console.WriteLine("What is your courage factor?");
-                courage = decimal.Parse(Console.ReadLine());
-                Heister heister = new Heister(name, skill, courage);
+                courageString = Console.ReadLine();
+
+                try{
+                    courageFactor= decimal.Parse(courageString);
+                }
+
+                catch(Exception ex){
+
+                    Console.WriteLine($"{courageString} is not a valid courage value. Using the default of 1.0.");
+                    courageFactor = 1.0M;
+                }
+                
+                Heister heister = new Heister(name, skillLevel, courageFactor);
                 heisterList.Add(heister);
             }
 
